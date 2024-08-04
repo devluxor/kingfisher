435a9c15ccb39037301be87924c0d1cab68decca
Lucas S <lucas.sorribes@gmail.com>
2024-08-04 19:33:09 +0200

    add middleware functions

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -4,6 +4,23 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-export default {
+const errorHandler = (error, request, response, next) => {
+  logger.error(error)
+
+  // Handle specific error types
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message }) //!
+  }
+
+  // Handle generic errors
+  response.status(500).json({ error: 'Internal Server Error' });
+
+  next(error)
+}
+
+export {
   unknownEndpoint,
+  errorHandler
 }
