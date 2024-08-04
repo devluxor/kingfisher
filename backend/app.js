61a8b97d84116d2ac4edc91d764b6a2011c472d6
Lucas S <lucas.sorribes@gmail.com>
2024-08-04 19:33:20 +0200

    add initial app.js structure

--- backend/app.js
+++ backend/app.js
@@ -10,16 +10,23 @@ import config from './utils/config.js'
 import morgan from 'morgan'
 
 // custom middleware (error handlers, unknown route handler, custom loggers, etc)
-import middleware from './utils/middleware.js'
+import {unknownEndpoint, errorHandler} from './utils/middleware.js'
 import logger from './utils/logger.js'
+import apiRouter from './controllers/apiRouter.js'
+import nestRouter from './controllers/nestRouter.js'
 
 const app = express()
 
-// This adds a custom token to the logger 'morgan',
-// so the body of the request sent to the server is shown on the terminal
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
+if (process.env.NODE_ENV === 'development') {
+  console.log('🎑 Developing!')
+
+  // This adds a custom token to the logger 'morgan',
+  // so the body of the request sent to the server is shown on the terminal
+  morgan.token('body', req => {
+    return JSON.stringify(req.body)
+  })
+
+}
 
 app.use(cors())
 app.use(express.json())
@@ -28,5 +35,10 @@ app.use(morgan(':method :url :status :body'))
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api', apiRouter)
+app.use('/', nestRouter)
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 export default app
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const PORT = process.env.PORT
+const WS_PORT = process.env.WS_PORT
 
 export default {
   PORT,
+  WS_PORT
 }
