593fcd18c0cb2fe09e7d29f4474bcf23dcef8e52
devluxor <lucas.sorribes@gmail.com>
2024-08-06 01:16:19 +0200

    update routers to code changes and new modules

--- backend/controllers/apiRouter.js
+++ backend/controllers/apiRouter.js
@@ -1,48 +1,38 @@
-import e, { Router } from "express";
+import { Router } from "express";
 import short from 'short-uuid';
+import DBSimulator from "./DBSimulator.dev.js";
 
 const apiRouter = Router()
 
-let id = 0
-export let nests = {}
-
-
-apiRouter.get('/nest/:nestId', async (req, res) => {
+// to delete later? 
+apiRouter.get('/nests/all', async (req, res) => {
   const nestId = req.params.nestId
-  const nest = nestDBSimulator('get', nestId)
-  // if (nests[nestId]) {
+  const nests = nestDBSimulator('getAll', nestId)
+  res.status(200).send(nests)
+})
+
+apiRouter.get('/nests/:nestId/', async (req, res) => {
+  const nestId = req.params.nestId
+  const nest = DBSimulator('get', nestId)
+
   if (nest) {
     return res.status(200).send(nest)
   }
-  console.log(nests)
+
   res.status(404).send({
-    error: `there is no nest with that id, or the id is invalid: ${nestId}`
+    error: `There is no nest with that id, or the id is invalid: ${nestId}`
   })
 })
 
 apiRouter.post('/createNest', async (req, res, next) => {
   try {
     const nestId = short.generate()
-    const newNest = {id: id++, ip: req.ip, hostName: req.hostname, requests: []}
-    nestDBSimulator('newNest', nestId, newNest  )
-    // nests[nestId] = {id: id++, ip: req.ip, hostName: req.hostname, requests: []}
+    const newNest = {id: nestId, ip: req.ip, hostName: req.hostname, requests: []}
+    DBSimulator('newNest', nestId, newNest  )
     res.status(200).send({ nestId })
   } catch (e) {
     next(e); // Pass the error to the next middleware (error handler, etc.)
   }
 })
-
-export const nestDBSimulator = (mode, nestId, newNest, req) => {
-  if (mode === 'newNest') {
-    nests[nestId] = newNest
-    return nestId
-  } else if (mode === 'get' && nests[nestId]){
-    return nests[nestId]
-  } else if (mode === 'addReq' && nests[nestId]) {
-    nests[nestId].requests.push(req)
-    return nestId
-  }
-}
-
 
 export default apiRouter
--- backend/controllers/nestRouter.js
+++ backend/controllers/nestRouter.js
@@ -1,51 +1,31 @@
 import { Router } from "express";
-import config from "../utils/config.js";
-import { WebSocketServer } from "ws";
 import { temporaryNestIdValidator } from "../utils/middleware.js";
-import { nestDBSimulator, nests } from "./apiRouter.js";
+import DBSimulator from "./DBSimulator.dev.js";
+import initializeWSServer from "./wsServer.js";
+import { dateFormatter } from "../utils/others.js";
 
 const nestRouter = Router()
-const clients = {}
+const wsClients = initializeWSServer()
 
+// This route represents the main endpoint
 nestRouter.all('/:nestId', temporaryNestIdValidator, async (req, res) => {
   const nestId = req.params.nestId;
   const method = req.method;
   const headers = req.headers;
   const body = req.body;
+  const timeOfArrival = new Date()
   // let request = await requestService.createRequest(nestId, method, path, headers, body)
-  const requestInfo = { nestId, method, headers, body } // add path later
-  
-  // if (nests[nestId]) {
-  //   nests[nestId].requests.push(requestInfo)
-  // }
+  const requestInfo = { nestId, timeOfArrival, method, headers, body } // add path later
 
-  nestDBSimulator('addReq', nestId, null, requestInfo)
+  DBSimulator('addReq', nestId, null, requestInfo)
 
-  if (clients['id1']) {
-    console.log('🌈message sent!')
-    clients['id1'].send(`${new Date().toString().slice(0,25)}, ${method}, ${JSON.stringify(body)}`);
+  const clients = wsClients()
+  if (clients[nestId]) {
+    console.log('🌈message sent from the server!')
+    clients[nestId].send(`${dateFormatter(timeOfArrival)}, ${method}, ${JSON.stringify(body)}`);
   }
 
   res.status(200).send('Request received');
-})
-
-const wsServer = new WebSocketServer({port: config.WS_PORT});
-wsServer.on("connection", (ws, req) => {
-  console.log('📯 Web Socket Server connected!')
-  // let nestId;
-
-  // does this mean that when a connection is made from the frontend,
-  // a message is sent to this server with the id???
-
-  // ws.on('message', (message) => {
-    // nestId = message.toString();
-    clients['id1'] = ws;
-  // });
-  
-  ws.on("close", () => {
-    console.log('✖ Web Socket Server closed!')
-    delete clients['id1']
-  });
 })
 
 export default nestRouter
