8ffff946ccf5f7ee9780c554651ed2c65149b3dc
devluxor <lucas.sorribes@gmail.com>
2024-08-06 01:15:04 +0200

    separate DB simulator into different module

---
+++ backend/controllers/DBSimulator.dev.js
@@ -0,0 +1,20 @@
+let nests = {}
+
+export default (mode, nestId, newNest, req) => {
+  try {
+    if (mode === 'newNest') {
+      nests[nestId] = newNest
+      return nestId
+    } else if (mode === 'get' && nests[nestId]){
+      return nests[nestId]
+    } else if (mode === 'addReq' && nests[nestId]) {
+      nests[nestId].requests.push(req)
+      return nestId
+    } else if (mode === 'getAll') {
+      return nests
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
+}
