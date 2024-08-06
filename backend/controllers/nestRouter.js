5bd9bd8e6d3a132bc268e0af2210d2939c347180
devluxor <lucas.sorribes@gmail.com>
2024-08-06 14:08:08 +0200

    feat(nestRouter): add path to request objects

--- backend/controllers/nestRouter.js
+++ backend/controllers/nestRouter.js
@@ -8,24 +8,25 @@ const nestRouter = Router()
 const wsClients = initializeWSServer()
 
 // This route represents the main endpoint
-nestRouter.all('/:nestId', temporaryNestIdValidator, async (req, res) => {
-  const nestId = req.params.nestId;
-  const method = req.method;
-  const headers = req.headers;
-  const body = req.body;
+nestRouter.all('/:nestId*', temporaryNestIdValidator, async (req, res) => {
+  const nestId = req.params.nestId
+  const method = req.method
+  const path = req.params[0]
+  const headers = req.headers
+  const body = req.body
   const timeOfArrival = new Date()
   // let request = await requestService.createRequest(nestId, method, path, headers, body)
-  const requestInfo = { nestId, timeOfArrival, method, headers, body } // add path later
+  const requestInfo = { nestId, timeOfArrival, method, path, headers, body } // add path later
 
   DBSimulator('addReq', nestId, null, requestInfo)
 
   const clients = wsClients()
   if (clients[nestId]) {
     console.log('🌈message sent from the server!')
-    clients[nestId].send(`${dateFormatter(timeOfArrival)}, ${method}, ${JSON.stringify(body)}`);
+    clients[nestId].send(`${dateFormatter(timeOfArrival)}, ${method}, ${path}, ${JSON.stringify(body)}`)
   }
 
-  res.status(200).send('Request received');
+  res.status(200).send('Request received')
 })
 
 export default nestRouter
