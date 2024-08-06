4b4cefd1681d8853c270d1ecfbe8c213875c7d5e
devluxor <lucas.sorribes@gmail.com>
2024-08-06 15:12:58 +0200

    fix(script): add informative messages to build-fontend.sh

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -3,6 +3,8 @@
 echo "🏗 Building frontend..."
 npm run build
 echo $1 | sudo -S rm -rf /var/www/kingfisher/html
+echo "Previous build destroyed"
 echo $1 | sudo -S mkdir /var/www/kingfisher/html
 echo $1 | sudo -S cp -r ./dist/* /var/www/kingfisher/html/
+echo "Build installed successfully"
 echo "👋 Goodbye!"
