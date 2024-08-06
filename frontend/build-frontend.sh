be706fe9b12b512ada6630bb7fe5deb71f429639
devluxor <lucas.sorribes@gmail.com>
2024-08-06 15:08:55 +0200

    fix(script): fix typo in build-frontend.sh

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -5,4 +5,4 @@ npm run build
 # echo $1 | sudo -S rm -rf /var/www/kingfisher/html
 # echo $1 | mkdir /var/www/kingfisher/html
 # echo $1 | cp -r ./dist/* /var/www/kingfisher/html/
-echo "Goodbye!
+echo "Goodbye!"
