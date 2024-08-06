6aa3089c6d324eadb6bb8ff09674fd3ac450064c
devluxor <lucas.sorribes@gmail.com>
2024-08-06 15:07:56 +0200

    deployment: move build commands to script file

---
+++ frontend/build-frontend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Building frontend..."
+npm run build
+# echo $1 | sudo -S rm -rf /var/www/kingfisher/html
+# echo $1 | mkdir /var/www/kingfisher/html
+# echo $1 | cp -r ./dist/* /var/www/kingfisher/html/
+echo "Goodbye!
