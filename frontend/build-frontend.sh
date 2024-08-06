c3272d5e887f73a0116b5ffd1ea39d51782ebbc5
devluxor <lucas.sorribes@gmail.com>
2024-08-06 15:10:57 +0200

    feat: add password secret to script

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Building frontend..."
+echo "🏗 Building frontend..."
 npm run build
-# echo $1 | sudo -S rm -rf /var/www/kingfisher/html
-# echo $1 | mkdir /var/www/kingfisher/html
-# echo $1 | cp -r ./dist/* /var/www/kingfisher/html/
-echo "Goodbye!"
+echo $1 | sudo -S rm -rf /var/www/kingfisher/html
+echo $1 | sudo -S mkdir /var/www/kingfisher/html
+echo $1 | sudo -S cp -r ./dist/* /var/www/kingfisher/html/
+echo "👋 Goodbye!"
