a1f38c0249bfbc6dc004d0fc5faa02911e96a69d
devluxor <lucas.sorribes@gmail.com>
2024-08-08 15:26:15 +0200

    add router wrapper to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   import.meta.env.DEV ?   
-    <React.StrictMode><App /></React.StrictMode> :
-    <App />
+    <React.StrictMode><Router><App /></Router></React.StrictMode> :
+    <Router><App /></Router>
 )
 
