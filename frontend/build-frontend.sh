88c38dc8b9be9b139bad15706a3374ff4b3859c2
devluxor <lucas.sorribes@gmail.com>
2024-08-09 16:44:20 +0200

    refactor(script): update echo messages in build-frontend.sh

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -7,5 +7,5 @@ echo $1 | sudo -S rm -rf /var/www/kingfisher/html
 echo "Previous build destroyed"
 echo $1 | sudo -S mkdir /var/www/kingfisher/html
 echo $1 | sudo -S cp -r ./dist/* /var/www/kingfisher/html/
-echo "Build installed successfully"
+echo "New build installed successfully and ready to be served"
 echo "👋 Goodbye!"
