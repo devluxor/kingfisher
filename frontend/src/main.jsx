5f6674141b1acdbb39b1af6bb1dee4ca71d7084f
devluxor <lucas.sorribes@gmail.com>
2024-08-09 15:30:25 +0200

    feat(front): add basename property to Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter as Router } from 'react-router-dom'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   import.meta.env.DEV ?   
-    <React.StrictMode><Router><App /></Router></React.StrictMode> :
-    <Router><App /></Router>
+    <React.StrictMode><Router basename='/kingfisher'><App /></Router></React.StrictMode> :
+    <Router basename='/kingfisher'><App /></Router>
 )
 
