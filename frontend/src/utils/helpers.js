abca7c9134ab248720660bbde9314e906b13da00
devluxor <lucas.sorribes@gmail.com>
2024-08-10 15:58:59 +0200

    refactor(front): add helpers

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,19 +1,5 @@
 import { testRequest } from "../services/testApi";
 
-export const deleteRequestsFromList = () => {
-  const list = document.getElementById("received-requests");
-  while (list?.firstChild) {
-    list.removeChild(list.lastChild);
-  }
-}
-
-export const deleteMessagesFromList = () => {
-  const list = document.getElementById("received-messages");
-  while (list?.firstChild) {
-    list.removeChild(list.lastChild);
-  }
-}
-
 export const test = async (nestId) => {
   try {
     await testRequest(nestId)
@@ -29,4 +15,31 @@ export const copyNestId = async (currentNestId) => {
   } catch (error) {
     console.error('Failed to copy text: ', error);
   }
+}
+
+export const setupHistory = () => {
+  if (!localStorage.getItem('kingfisherHistory')) {
+    localStorage.setItem('kingfisherHistory', JSON.stringify({}))
+  }
+}
+
+export const saveNestInLocalStorage = (nestId) => {
+  localStorage.setItem('kingfisherNest', nestId)
+}
+
+export const saveNestInHistory = (nestId) => {
+  const MAX_BYTE_SIZE = 4_999_900
+  const history = JSON.parse(localStorage.getItem('kingfisherHistory'))
+  const historySize = new Blob([history]).size
+  if (historySize >= MAX_BYTE_SIZE) {
+    alert(`
+      Warning! The history exceeds the maximum size allowed of 5 Mb, 
+      new entries will replace the older ones until the storage is manually cleared.
+    `)
+    const firstKey = Object.keys(history)
+    delete history[firstKey]
+  }
+
+  history[nestId] = true
+  localStorage.setItem('kingfisherHistory', JSON.stringify(history))
 }
