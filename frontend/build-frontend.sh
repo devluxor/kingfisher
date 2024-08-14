e2be435b1457f90dcec6aa54cad12578b6b8caca
devluxor <lucas.sorribes@gmail.com>
2024-08-14 09:54:41 +0200

    feat(script): add pm2 restart command to building script

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -5,7 +5,9 @@ npm install # if new packages have been installed during development
 npm run build
 echo $1 | sudo -S rm -rf /var/www/kingfisher/html
 echo "Previous build destroyed"
-echo $1 | sudo -S mkdir /var/www/kingfisher/html
-echo $1 | sudo -S cp -r ./dist/* /var/www/kingfisher/html/
+sudo mkdir /var/www/kingfisher/html
+sudo cp -r ./dist/* /var/www/kingfisher/html/
 echo "New build installed successfully and ready to be served"
+echo "Restarting services..."
+pm2 restart 2
 echo "👋 Goodbye!"
