9ace497c0e5223d1bd497feee3ee37d736180102
devluxor <lucas.sorribes@gmail.com>
2024-08-29 14:35:56 +0200

    debugging: add testing logs to inMemoryDB

--- backend/inMemoryDB.js
+++ backend/inMemoryDB.js
@@ -6,14 +6,21 @@ const loadNest = (nestId) => {
 }
 
 const addNewWSConnection = (nestId, wsServerURL) => {
-  if (!nests[nestId].wsConnections[wsServerURL]) {
-    nests[nestId].wsConnections[wsServerURL] = []
+  console.log('add new ws connection')
+  console.log(nests)
+  try {
+    if (!nests[nestId]?.wsConnections[wsServerURL]) {
+      nests[nestId].wsConnections[wsServerURL] = []
+    }
+  } catch (e) {
+    console.error(e)
   }
   
   return nestId
 }
 
 const addNewWSMessage = (nestId, wsServerURL, data) => {
+  console.log('add new ws message')
   nests[nestId].wsConnections[wsServerURL].push(data)
 }
 
