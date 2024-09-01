13f5d1bb890dcd7cf332244b9bc33efe566c1bd1
devluxor <lucas.sorribes@gmail.com>
2024-09-01 16:18:59 +0200

    feat(script): script always asks password in sudo commands and .bash_history ignores them for extra security

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# "HISTIGNORE" means to not save this command into the history. 
+# That is the history in memory or "~/.bash_history" file.
+export HISTIGNORE='*sudo -S*'
+
 echo "🏗 Building frontend..."
 npm install # if new packages have been installed during development
 npm audit fix
 npm run build
-echo $1 | sudo -S rm -rf /var/www/kingfisher/html
+
+# sudo:
+# -S means to use stdin for the password,
+# -k means to ignore cached credentials to force sudo to always ask. 
+# This is for consistent behavior.
+
+echo $1 | sudo -S -k rm -rf /var/www/kingfisher/html
 echo "Previous build destroyed"
-sudo mkdir /var/www/kingfisher/html
-sudo cp -r ./dist/* /var/www/kingfisher/html/
+echo $1 | sudo -S -k mkdir /var/www/kingfisher/html
+echo $1 | sudo -S -k cp -r ./dist/* /var/www/kingfisher/html/
 echo "New build installed successfully and ready to be served"
 echo "Restarting services..."
 pm2 restart Kingfisher
