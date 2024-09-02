2751dd3a89476c3abdfdb7e3b1f075adf69cfd69
devluxor <lucas.sorribes@gmail.com>
2024-09-02 19:10:33 +0200

    fix(front.): app displays correct message for new nests without any requests

--- frontend/src/components/RequestsList.jsx
+++ frontend/src/components/RequestsList.jsx
@@ -1,15 +1,22 @@
+const developmentMode = import.meta.env.DEV
+
 const RequestsList = ({requests}) => {
-  console.log('RequestList Rendered')
+  developmentMode && console.log('RequestList Rendered')
 
   if (!requests) return <h3>Loading Requests</h3>
 
+  const requestsReceived = requests.length > 0
+
   return (
     <> 
-      <h4>List of received requests:</h4>
-      {requests.length === 0 ? 'No requests yet' :
-        <div><h4>{requests.length} Received Request{requests.length > 1 ? 's' : ''}</h4><ul id="received-requests">{
-          requests.map(r => <RequestListElement r={r} key={r.id}/>)
-        }</ul></div>
+      <h4>{ !requestsReceived ? 'No requests yet' : 'List of received requests:'}</h4>
+      {requestsReceived && 
+        <div>
+          <h4>{requests.length} Received Request{requests.length > 1 ? 's' : ''}</h4>
+          <ul id="received-requests">
+            {requests.map(r => <RequestListElement r={r} key={r.id}/>)}
+          </ul>
+        </div>
       }
     </>
   )
@@ -17,7 +24,6 @@ const RequestsList = ({requests}) => {
 
 const RequestListElement = ({r}) => {
   const arrivedOn = String(r.arrivedOn || r.arrived_on)
-  // const path = 
   return <li className="request">🎈 {arrivedOn} {r.method} {r.path? r.path : "/"}</li>
 }
 
